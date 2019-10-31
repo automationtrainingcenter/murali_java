1422e1af34ae4a52faa73f577dff8dbc8bdad8e4
automationtrainingcenter <atcsurya@gmail.com>
2019-10-31 10:57:01 +0530

    initial upload with hello world

---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("welcome to sunshine");
+	}
+	
+}
